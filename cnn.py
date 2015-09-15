b067264d37548758552f8a8969550ed1dc0607a2
dongchao <dongchao@xiaomi.com>
2015-09-15 13:32:20 +0800

    dev cnn, rcnn

--- cnn.py
+++ cnn.py
@@ -16,8 +16,8 @@ class CNN(object):
         """
         Params:
         -------
-        input_data: symbolic sentence tensor
-        type of input_data: theano.tensor.matrix
+        input_data: symbolic sentence tensor, a 3-dimension tensor
+        type of input_data: theano.tensor 3d tensor
 
         dim: the dimensions of word vector
         type of dim: int
@@ -30,33 +30,47 @@ class CNN(object):
 
 
         """
-        self.input_data = input_data.dimshuffle('x', 'x', 0, 1)
+        self.input_data = input_data
+        #self.input_data = input_data.dimshuffle('x', 'x', 0, 1)
         self.dim = dim
         self.n_feature_maps = n_feature_maps
         self.window_sizes = window_sizes
-        self.params = []
 
-        self.h = None
+        # params init
+        self.params = []
+        self.W_list = []
         for ws in window_sizes:
             # ws declare each window_size
             W_init = np.asarray(rng.uniform(low=-0.1,
-                                       high=0.1,
-                                       size=(self.n_feature_maps,
-                                             1,
-                                             ws,
-                                             self.dim)),
-                           dtype=theano.config.floatX)
+                                            high=0.1,
+                                            size=(self.n_feature_maps,
+                                                  1,
+                                                  ws,
+                                                  self.dim)),
+                                dtype=theano.config.floatX)
             W = theano.shared(value=W_init, name="W")
+            self.W_list.append(W)
             self.params.append(W)
-
-            conv_out = conv.conv2d(input=self.input_data, filters=W)
-            max_out = T.max(conv_out, axis=2).flatten()
-
-
-            self.h = max_out if self.h == None else \
-                          T.concatenate([self.h, max_out])
         b_init = np.asarray(np.zeros((self.n_feature_maps * len(self.window_sizes),), dtype=theano.config.floatX))
         self.b = theano.shared(value=b_init)
         self.params.append(self.b)
-        self.output = self.h + self.b
+
+
+        def _conv(word_vector):
+            word_matrix = word_vector.dimshuffle('x', 'x', 0, 1)
+            self.h = None
+
+            for i in xrange(len(window_sizes)):
+                ws = window_sizes[i]
+                conv_out = conv.conv2d(input=word_matrix, filters=self.W_list[i])
+                max_out = T.max(conv_out, axis=2).flatten()
+                self.h = max_out if self.h == None else \
+                         T.concatenate([self.h, max_out])
+                self.o = self.h + self.b
+                return self.o
+
+        [self.output], _ = theano.scan(_conv,
+                                       sequences=self.input_data,
+                                       outputs_info=[None])
+
         return
--- rcnn.py
+++ rcnn.py
@@ -24,21 +24,19 @@ class RCNN(object):
                  window_sizes,
                  n_hidden,
                  n_out,
-                 h_prev,
-                 y_prev):
-
+                 learning_rate,
+                 learning_rate_decay
+                ):
         self.cnn = cnn.CNN(input_data=input_data,
                            rng=rng,
                            dim=dim,
                            n_feature_maps=n_feature_maps,
                            window_sizes=window_sizes)
         self.rnn = rnn.RNN(input_data=self.cnn.output,
-                           rng=rng,
-                           n_in = n_feature_maps*len(window_sizes),
+                           #rng=rng,
+                           n_input = n_feature_maps*len(window_sizes),
                            n_hidden=n_hidden,
-                           n_out=n_out,
-                           h_prev=h_prev,
-                           y_prev=y_prev,
+                           n_output=n_out,
                            activation=T.nnet.sigmoid)
 
         self.h = self.rnn.h
@@ -51,3 +49,97 @@ class RCNN(object):
         self.loss = self.rnn.loss
         self.error = self.rnn.error
         return
+
+
+def start_rcnn(dim,
+               n_feature_maps,
+               window_sizes,
+               n_hidden,
+               n_out,
+               learning_rate=0.01,
+               learning_rate_decay=0.1,
+               n_epochs=1,
+               validation_frequency=1000):
+
+
+    # log the params
+    logging.info("start the rcnn process!")
+    logging.info("the params of this turn is:")
+    logging.info("n_feature_maps: %d" % (n_feature_maps))
+    logging.info("window_sizes: {}".format(window_sizes))
+    logging.info("n_hidden: %d" % (n_hidden))
+    logging.info("learning_rate: %f, learning_rate_decay: %f, validation_frequency: %d" % (learning_rate, learning_rate_decay, validation_frequency))
+
+    logging.info("initialize")
+    x_var = T.ftensor3('x_var')
+    y_var = T.vector('y_var')
+    lr_var = T.vscalar("lr_var")
+    rcnn = RCNN(rng=rng,
+                input_data=x_var,
+                dim=dim,
+                n_feature_maps=n_feature_maps,
+                window_sizes=window_sizes,
+                n_hidden=n_hidden,
+                n_out=n_out)
+
+    # definite loss function and SGD funcition
+    cost = rcnn.loss(y_var)
+    error = rcnn.error(label_var)
+    gparams = [T.grad(cost, param) for param in rcnn.params]
+    sgd_updates = [(param, param - lr_var * gparam) for (param, gparam) in zip(rcnn.params, gparams)]
+
+    # definite train model
+    train_model = theano.function(inputs=[x_var, y_var, lr_var],
+                                  outputs=[],
+                                  updates=sgd_updates)
+    compute_loss = theano.function(inputs=[x_var, y_var],
+                                   outputs=[cost])
+    compute_error = theano.function(inputs=[x_var, y_var],
+                                    outputs=[error])
+
+    logging.info("begin to train")
+    # load the dataset
+    train_x, train_y, valid_x, valid_y, test_x, test_y = data_process.load_utterance_dataset()
+
+    n_train = len(train_x)
+    n_valid = len(valid_x)
+    n_test = len(test_x)
+    epoch = 0
+
+    while epoch < n_epochs:
+        epoch += 1
+        for idx in xrange(n_train):
+            train_model(train_x[idx],
+                        train_y[idx],
+                        learning_rate)
+
+            iter = (epoch-1) * n_train + idx + 1
+            if iter % validation_frequency == 0:
+
+                error_cnt = 0
+                cost_cnt = 0
+                for vdx in xrange(n_valid):
+                    valid_cost = compute_loss(valid_x[vdx], valid_y[vdx])
+                    valid_error = compute_error(valid_x[vdx], valid_y[vdx])
+                    cost_cnt += valid_cost
+                    error_cnt += valid_error
+
+                # valid cost and error stats
+                error_cnt = error_cnt * 1.0 / n_valid
+                cost_cnt = cost_cnt * 1.0 / n_valid
+                logging.info("Epoch %d, seq %i/%i, error_cnt: %f, cost_cnt: %f" \
+                             %(epoch, idx, n_train, error_cnt, cost_cnt))
+
+        learning_rate = learning_rate * (1-learning_rate_decay)
+
+if __name__ == "__main__":
+
+    log_file = sys.argv[1]
+    logging.basicConfig(
+        level=logging.DEBUG, format='[%(asctime)s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S', filename=log_file, filemode='w')
+    start_rnn(dim=300,
+              n_feature_maps=300,
+              window_sizes=(2, 3,4 , 5),
+              n_hidden=300,
+              n_out=43)
