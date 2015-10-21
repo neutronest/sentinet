4d2d0757ccc02437f6c3e534b2fa66c6e871b864
dongchao <dongchao@xiaomi.com>
2015-10-21 14:24:42 +0800

    add rcnn test

---
+++ test/test_rcnn.py
@@ -0,0 +1,24 @@
+## -*- coding: utf-8 -*-
+import pdb
+import theano
+import theano.tensor as T
+from theano.tensor.nnet import conv
+import numpy as np
+import sys
+
+sys.path.append("../src")
+import rnn
+import cnn
+import rcnn
+import test_cnn
+import test_rnn
+
+def test_rcnn():
+    """
+    """
+
+    
+    return
+
+if __name__ == "__main__":
+    test_rcnn()
