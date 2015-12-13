599de7a911b83d6310de6b47aab352ea12b89e62
dongchao <dongchao@xiaomi.com>
2015-12-13 16:50:51 +0800

    cnn

--- src/models.py
+++ src/models.py
@@ -202,7 +202,7 @@ class SingleModel(object):
                                  True)
 
         self.level2_hidden = getattr(self.model, "level2_hidden", 100)
-        self.relations = getattr(self.model, 'relations', T.fvector('relations'))
+        self.relations = getattr(self.model, 'relations', T.ivector('relations'))
         self.th = getattr(self.model, 'th', T.fvector('th'))
         self.h0 = getattr(self.model, 'h0', T.fvector('h0'))
         self.c0 = getattr(self.model, 'c0', T.fvector('c0'))
