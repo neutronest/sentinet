70dbcab7f25819897fd38662f68e4cd5525a72c2
dongchao <dongchao@xiaomi.com>
2015-12-14 19:19:07 +0800

    rm sh

--- src/backup/microblog_hrnn.sh
+++ src/backup/microblog_hrnn.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cmd="python main.py
     --experiment=microblog
-    --model_name=lstm_model
-    --level1_model_name=lstm_model
-    --level2_model_name=none
+    --model_name=srnn_trnn_model
+    --level1_model_name=srnn_model
+    --level2_model_name=trnn_model
     --dataset_name=microblog
-    --log_path=../logs/v2_lstm.txt
-    --word_dim=100
-    --level1_input=100
+    --log_path=../logs/weibo_hrnn_gpu_1.txt
+    --word_dim=64
+    --level1_input=64
     --level1_hidden=100
     --level2_input=100
     --level2_hidden=100
@@ -16,14 +16,14 @@ cmd="python main.py
     --cnn_window_sizes=2@3@4
     --if_dropout=dropout
     --optimizer_method=adadelta
-    --learning_rate=1
+    --learning_rate=0.1
     --batch_type=minibatch
     --batch_size=10
     --n_epochs=100
     --train_pos=0@1@2
     --valid_pos=3
     --test_pos=4
-    --valid_frequency=200"
+    --valid_frequency=1000"
 
 if [ $# -eq 0 ]; then
     THEANO_FLAGS="mode=FAST_RUN,floatX=float32" $cmd
